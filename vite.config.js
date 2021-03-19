241b395ede09fb243266ee2991ed81103a27df1a
qhx0807 <820240134@qq.com>
2021-03-19 16:17:21 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,31 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import vitePluginImp from 'vite-plugin-imp';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    reactRefresh(),
-    vitePluginImp({
-      libList: [
-        {
-          libName: 'antd',
-          style: name => `antd/lib/${name}/style/index.less`
-        }
-      ]
-    })
-  ],
+  plugins: [reactRefresh()],
   css: {
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
-        // modifyVars: {  //
-        //   "primary-color": "#1DA57A",
-        // }
-      }
-    }
+        modifyVars: {
+          // 自定义主题色
+          'primary-color': '#1DA57A',
+        },
+      },
+    },
   },
   resolve: {
     alias: [
       {
         find: /^~/,
         replacement: path.resolve(__dirname, 'src'),
-      }
-    ]
+      },
+    ],
   },
   server: {
-    open: true
-  }
+    open: true,
+  },
 });
